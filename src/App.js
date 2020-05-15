49b48ce4abe230da55e013dc85e02236aed6ee48
Pedro <pedro.cerdera@hotmail.com>
2020-05-15 17:49:59 -0300

    install fonts

--- src/App.js
+++ src/App.js
@@ -2,11 +2,15 @@ import React from 'react';
 import {StatusBar} from 'react-native';
 import {Redux} from './redux';
 import {WeatherForecastScreen} from './screens';
+import styleguide from './styleguide';
 
 const App = () => {
   return (
     <Redux>
-      <StatusBar barStyle="light-content" />
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor={styleguide.colors.background}
+      />
       <WeatherForecastScreen />
     </Redux>
   );
--- src/screens/WeatherForecast/WeatherForecastScreen.js
+++ src/screens/WeatherForecast/WeatherForecastScreen.js
@@ -1,10 +1,16 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import {Text, View} from 'react-native';
 import {useDispatch, useSelector} from 'react-redux';
 import {useLocation} from '../../hooks';
 import {Actions} from '../../reducers';
+import {
+  DefaultSpacingContainer,
+  SafeAreaContainer,
+  CardContainer,
+} from '../../components';
 
 const WeatherForecastScreen = () => {
+  const [currentWeather, setCurrentWeather] = useState(0);
   const {coordinates} = useLocation();
   const {weathers} = useSelector((store) => ({
     weathers: store.WeatherReducer.weathers,
@@ -18,9 +24,38 @@ const WeatherForecastScreen = () => {
   }, [coordinates, dispatch]);
 
   return (
-    <View>
-      <Text>{JSON.stringify(weathers)}</Text>
-    </View>
+    <SafeAreaContainer>
+      <DefaultSpacingContainer>
+        <CardContainer
+          style={{flex: 1, alignItems: 'center', paddingVertical: 20}}>
+          <Text style={{fontFamily: 'Poppins-Bold'}}>
+            {weathers[currentWeather].date_br}
+          </Text>
+          <View
+            style={{
+              flex: 1,
+              backgroundColor: 'tomato',
+              justifyContent: 'center',
+              alignItems: 'center',
+              marginVertical: 20,
+            }}>
+            <View
+              style={{
+                height: 250,
+                width: 250,
+                borderRadius: 125,
+                backgroundColor: 'green',
+              }}
+            />
+          </View>
+          <View>
+            <Text style={{fontFamily: 'Poppins-Bold'}}>
+              {weathers[currentWeather].temperature.max}
+            </Text>
+          </View>
+        </CardContainer>
+      </DefaultSpacingContainer>
+    </SafeAreaContainer>
   );
 };
 
